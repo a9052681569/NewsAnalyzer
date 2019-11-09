c1a20b0c7688946528654ececb92451deeffe8a6
Andrew <a9052681569@gmail.com>
2019-11-09 16:07:07 +0300

    все страницы проходят валидацию без ошибок, реализована валидация формы, альтернативные шрифты, эффекты на интерактивных элементах

--- src/blocks/search/__result/searchResult.js
+++ src/blocks/search/__result/searchResult.js
@@ -4,7 +4,8 @@ import resSuccess from './__success/searchSuccess';
 const preloader = new Preloader(document.querySelector('.search__result'));
 let res = true;
 // после клика по кнопке поиска отрисовываем нужные результаты
-export default document.querySelector('.search__bar-button').addEventListener('click', () => {
+export default document.forms.search.addEventListener('submit', (event) => {
+    event.preventDefault();
     // вместо res будет запрос на сервер, если он успешен - отрисовываем результаты
     if (res) {
         preloader.preload();
